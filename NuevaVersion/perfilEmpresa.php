344d148dd0aa1720df40645ec3c51766d9bd3970
tanomartin05 <tanomartin05@gmail.com>
2013-08-13 16:31:52 +0000

    Cambio en el formulario default.html se agrego el welcome ahi. TODO: no anda el formulario del perfil de Empresa.

--- NuevaVersion/perfilEmpresa.php
+++ NuevaVersion/perfilEmpresa.php
@@ -1,6 +1,6 @@
 <?php session_save_path("sesiones");
 session_start();
-
+include('lib/php/conexion.php');
 // Directorio Raíz de la app
 // Es utilizado en templateEngine.inc.php
 $root = '';
@@ -8,9 +8,14 @@ $root = '';
 if(!empty($_SESSION) && $_SESSION['userLogin'] == true){
 	// Incluyo el template engine
 	include('includes/templateEngine.inc.php');
+	
+	$cuit = $_SESSION['userCuit'];
+	$consulta = "SELECT * FROM empresa WHERE nrcuit=$cuit";
+	$respuesta = $mysqli -> query($consulta);
+	$empresaData = $respuesta -> fetch_assoc();
 
 	// Cargo la plantilla
-	$twig->display('perfilEmpresa.html',array("userName" => $_SESSION['userNombre'], "userID" => $_SESSION['userID']));
+	$twig->display('perfilEmpresa.html',array("userName" => $_SESSION['userNombre'], "cuit" => $_SESSION['userCuit'], "nombre" => $empresaData['nombre']));
 }
 else{
 	header("Location:login.php");
