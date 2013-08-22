b9c5dfc7346586d7490573e58925ce552c609f58
tanomartin05 <tanomartin05@gmail.com>
2013-08-22 15:59:40 +0000

    Agregado de nuevo familiar TODO: agregar nuevo titular.

---
+++ NuevaVersion/grabanuevoFamiliar.php
@@ -0,0 +1,47 @@
+<?php session_save_path("sesiones");
+session_start();
+include('lib/php/verificaSesion.php');
+include('lib/php/verificaConexion.php');
+include('lib/php/funciones.php');
+
+$datos = array_values($_POST);
+
+//var_dump($datos);
+
+$nrcuit =  $_SESSION['userCuit'];
+$nrcuil =  $_GET['cuil'];
+
+$apellido = strtoupper($datos[0]);
+$nombre = strtoupper($datos[1]);
+$codpar = $datos[2];
+$sexo = $datos[3];
+$fecnac = fechaParaGuardar($datos[4]);
+$fecing = fechaParaGuardar($datos[5]);
+$tipdoc = $datos[6];
+$numdoc = $datos[7];
+$benefi = $datos[8];
+
+
+//Ejecucion de la sentencia SQL
+$sqlNuevoFamiliar = "INSERT INTO familia(nrcuit,nrcuil,nombre,apelli,codpar,ssexxo,fecnac,fecing,tipdoc,nrodoc,benefi) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+
+
+try {
+	if ($stmt = $mysqli->prepare($sqlNuevoFamiliar)) {
+		$stmt->bind_param('sssssssssss', $nrcuit, $nrcuil, $nombre, $apellido, $codpar, $sexo, $fecnac, $fecing, $tipdoc, $numdoc, $benefi);
+		
+		$stmt->execute();
+		$stmt->close();
+		
+		$pagina = "perfilEmpleado.php?cuil=$nrcuil";
+		Header("Location: $pagina"); 
+	} else {
+		 die("ERROR MYSQLI: <br>".$mysqli->error );
+	}
+} 
+catch(Exception $e){
+    $mysqli->rollback();
+    die("ERROR MYSQLI: <br>".$e->getMessage() );
+}
+
+?>
---
+++ NuevaVersion/nuevoFamiliar.php
@@ -0,0 +1,22 @@
+<?php session_save_path("sesiones");
+session_start();
+	include('lib/php/verificaSesion.php');
+	include('lib/php/verificaConexion.php');
+	include('lib/php/funciones.php');
+	$root = '';
+
+	// Incluyo el template engine
+	include('includes/templateEngine.inc.php');
+	
+	$cuit = $_SESSION['userCuit'];
+	$cuil = $_GET['cuil'];
+	
+	$consulta = "SELECT * FROM empleados WHERE nrcuil = $cuil and nrcuit = $cuit";
+	$respuesta = $mysqli -> query($consulta);
+	$titularData = $respuesta -> fetch_assoc();
+	$titular = (object) array('cuil' =>  $titularData['nrcuil'], 'apellido' => $titularData['apelli'], 'nombre' => $titularData['nombre']);	
+	
+	// Cargo la plantilla
+	$twig->display('nuevoFamiliar.html',array("userName" => $_SESSION['userNombre'], "titular" => $titular));
+
+?>
