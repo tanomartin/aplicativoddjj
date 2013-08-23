3faf55ed9662d80f7f2c4d35800ab88cecde8c5a
tanomartin05 <tanomartin05@gmail.com>
2013-08-23 14:42:35 +0000

    Cambio en los html, se separaron los titulos del cuerpo Agregado de pantallas de carga de archivos de modulog TODO: Controlar los archivos de modulog.

---
+++ NuevaVersion/cargaArchivoFam.php
@@ -0,0 +1,13 @@
+<?php session_save_path("sesiones");
+session_start();
+
+	include('lib/php/verificaSesion.php');
+	include('lib/php/verificaConexion.php');
+	$root = '';
+	// Incluyo el template engine
+	include('includes/templateEngine.inc.php');
+	
+	// Cargo la plantilla
+	$twig->display('cargaArchivoFam.html',array("userName" => $_SESSION['userNombre']));
+
+?>
