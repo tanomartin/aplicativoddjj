7fb60e8256cdedf7a51095fec51b72e2a7b18eef
tanomartin05 <tanomartin05@gmail.com>
2013-11-19 17:23:58 +0000

    arreglo error de total de remuneracion o alicuota dependiendo si es no remunerativo o comun

--- NuevaVersion/imprimirDDJJ.php
+++ NuevaVersion/imprimirDDJJ.php
@@ -39,14 +39,16 @@ if($tipo == "sindocu") {
    		$sentencia->execute();
     	$sentencia->bind_result($nrcuil, $remune, $apo060, $apo100, $apo150);
 		$i = 0;
+		$totRemu = 0;
 		while ($sentencia->fetch()) {
 			$detalle[$i] = array('nrcuil' => $nrcuil, 'nombre' => '', 'fecini' => '', 'remune' => $remune, 'apo060' => $apo060, 'apo100' => $apo100, 'apo150' => $apo150);
 			$i = $i + 1;
+			$totRemu = $totRemu + $remune;
 		}
 		$sentencia->close();
 	}
 
-	$totales = "select periodos.anio, periodos.descripcion, ddjj.remune, ddjj.apo060, ddjj.apo100, ddjj.apo150,  ddjj.totapo, ddjj.recarg, ddjj.totapo+ddjj.recarg as totdep
+	$totales = "select periodos.anio, periodos.mes, periodos.descripcion, ddjj.remune, ddjj.apo060, ddjj.apo100, ddjj.apo150,  ddjj.totapo, ddjj.recarg, ddjj.totapo+ddjj.recarg as totdep
 					from ddjj, periodos 
 					where 
 					ddjj.nrcuit = $nrcuit and 
@@ -55,7 +57,8 @@ if($tipo == "sindocu") {
 					ddjj.perano = periodos.anio and
 					ddjj.permes = periodos.mes";
 	$respuesta = $mysqli -> query($totales);
-	$totalData = $respuesta -> fetch_assoc();
+	$totalrow = $respuesta -> fetch_assoc();
+	$totalData = array('mes' => $totalrow['mes'], 'descripcion' => $totalrow['descripcion'], 'anio' => $totalrow['anio'], 'remune' => number_format($totRemu,2,',','.'), 'apo060' => $totalrow['apo060'], 'apo100' => $totalrow['apo100'], 'apo150' => $totalrow['apo150'], 'totapo' => $totalrow['totapo'], 'recarg' => $totalrow['recarg'], 'totdep' => $totalrow['totdep']);
 }
 
 if($tipo == "condocu") {
@@ -64,14 +67,16 @@ if($tipo == "condocu") {
    		$sentencia->execute();
     	$sentencia->bind_result($nrcuil, $remune, $apo060, $apo100, $apo150);
 		$i = 0;
+		$totRemu = 0;
 		while ($sentencia->fetch()) {
 			$detalle[$i] = array('nrcuil' => $nrcuil, 'nombre' => '', 'fecini' => '', 'remune' => $remune, 'apo060' => $apo060, 'apo100' => $apo100, 'apo150' => $apo150);
+			$totRemu = $totRemu + $remune;
 			$i = $i + 1;
 		}
 		$sentencia->close();
 	}
 
-	$totales = "select periodos.anio, periodos.descripcion, ddjjcondocu.remune, ddjjcondocu.apo060, ddjjcondocu.apo100, ddjjcondocu.apo150,  ddjjcondocu.totapo, ddjjcondocu.recarg, ddjjcondocu.totapo+ddjjcondocu.recarg as totdep
+	$totales = "select periodos.anio, periodos.mes, periodos.descripcion, ddjjcondocu.remune, ddjjcondocu.apo060, ddjjcondocu.apo100, ddjjcondocu.apo150,  ddjjcondocu.totapo, ddjjcondocu.recarg, ddjjcondocu.totapo+ddjjcondocu.recarg as totdep
 					from ddjjcondocu, periodos 
 					where 
 					ddjjcondocu.nrcuit = $nrcuit and 
@@ -80,7 +85,8 @@ if($tipo == "condocu") {
 					ddjjcondocu.perano = periodos.anio and
 					ddjjcondocu.permes = periodos.mes";
 	$respuesta = $mysqli -> query($totales);
-	$totalData = $respuesta -> fetch_assoc();
+	$totalrow = $respuesta -> fetch_assoc();
+	$totalData = array('mes' => $totalrow['mes'], 'descripcion' => $totalrow['descripcion'], 'anio' => $totalrow['anio'], 'remune' => number_format($totRemu,2,',','.'), 'apo060' => $totalrow['apo060'], 'apo100' => $totalrow['apo100'], 'apo150' => $totalrow['apo150'], 'totapo' => $totalrow['totapo'], 'recarg' => $totalrow['recarg'], 'totdep' => $totalrow['totdep']);
 }
 
 if($tipo == "valida") {
@@ -89,14 +95,16 @@ if($tipo == "valida") {
    		$sentencia->execute();
     	$sentencia->bind_result($nrcuil, $remune, $apo060, $apo100, $apo150);
 		$i = 0;
+		$totRemu = 0;
 		while ($sentencia->fetch()) {
 			$detalle[$i] = array('nrcuil' => $nrcuil, 'nombre' => '', 'fecini' => '', 'remune' => $remune, 'apo060' => $apo060, 'apo100' => $apo100, 'apo150' => $apo150);
+			$totRemu = $totRemu + $remune;
 			$i = $i + 1;
 		}
 		$sentencia->close();
 	}
 	
-	$totales = "select periodos.anio, periodos.descripcion, validas.remune, validas.apo060, validas.apo100, validas.apo150,  validas.totapo, validas.recarg, validas.totapo+validas.recarg as totdep
+	$totales = "select periodos.anio, periodos.mes, periodos.descripcion, validas.remune, validas.apo060, validas.apo100, validas.apo150,  validas.totapo, validas.recarg, validas.totapo+validas.recarg as totdep
 					from validas, periodos 
 					where 
 					validas.nrcuit = $nrcuit and 
@@ -105,7 +113,8 @@ if($tipo == "valida") {
 					validas.perano = periodos.anio and
 					validas.permes = periodos.mes";
 	$respuesta = $mysqli -> query($totales);
-	$totalData = $respuesta -> fetch_assoc();
+	$totalrow = $respuesta -> fetch_assoc();
+	$totalData = array('mes' => $totalrow['mes'], 'descripcion' => $totalrow['descripcion'], 'anio' => $totalrow['anio'], 'remune' => number_format($totRemu,2,',','.'), 'apo060' => $totalrow['apo060'], 'apo100' => $totalrow['apo100'], 'apo150' => $totalrow['apo150'], 'totapo' => $totalrow['totapo'], 'recarg' => $totalrow['recarg'], 'totdep' => $totalrow['totdep']);
 }
 
 for($i=0; $i < sizeof($detalle); $i++){
