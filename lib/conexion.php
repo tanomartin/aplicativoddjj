4a44de58cc01f11d5518ce2617199ba13f624192
tanomartin05 <tanomartin05@gmail.com>
2014-03-07 12:51:47 +0000

    Ignoro la conexion.php

--- lib/conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "XXXXX";
-$pass = "XXXXX";
-$db = mysql_connect($host,$user,$pass);
-mysql_select_db('ospimrem_aplicativo');
-$nrcuit = $_SESSION['nrcuit'];
-
-?>
