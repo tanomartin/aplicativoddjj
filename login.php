e56e0f1c58d212d51441a1e324189dc0a3c591a7
tanomartin05 <tanomartin05@gmail.com>
2015-04-22 17:18:09 +0000

    Cambios de login del aplicastivo viejo

--- login.php
+++ login.php
@@ -9,39 +9,38 @@ A:hover {text-decoration:underline; color:FCF63C}
 -->
 </style>
 <STYLE>
-BODY {SCROLLBAR-FACE-COLOR: #E4C192; 
-SCROLLBAR-HIGHLIGHT-COLOR: #CD8C34; 
-SCROLLBAR-SHADOW-COLOR: #CD8C34; 
-SCROLLBAR-3DLIGHT-COLOR: #CD8C34; 
-SCROLLBAR-ARROW-COLOR: #CD8C34; 
-SCROLLBAR-TRACK-COLOR: #CD8C34; 
-SCROLLBAR-DARKSHADOW-COLOR: #CD8C34
+BODY {
+	SCROLLBAR-FACE-COLOR: #E4C192;
+	SCROLLBAR-HIGHLIGHT-COLOR: #CD8C34;
+	SCROLLBAR-SHADOW-COLOR: #CD8C34;
+	SCROLLBAR-3DLIGHT-COLOR: #CD8C34;
+	SCROLLBAR-ARROW-COLOR: #CD8C34;
+	SCROLLBAR-TRACK-COLOR: #CD8C34;
+	SCROLLBAR-DARKSHADOW-COLOR: #CD8C34;
+	color: #000000;
 }
 .Estilo1 {
-	font-family: Verdana, Arial, Helvetica, sans-serif;
-	font-size: 14px;
+	color: #000099;
+	font-weight: bold;
 }
 </STYLE>
 <title>.: U.S.I.M.R.A. :.</title>
 </head>
 
 <body bgcolor="#E4C192" link="#D5913A" vlink="#CF8B34" alink="#D18C35">
-  <p align="center"><img border="0" src="img/top.jpg" width="700" height="120"></p>
-
-  <p style="word-spacing: 0; margin-top: 0; margin-bottom: 0"><font color="#E4C192">s</font></p>
-
-  <p style="word-spacing: 0; margin-top: 0; margin-bottom: 0"><font color="#E4C192">n</font></p>
-
-  <p style="word-spacing: 0; margin-top: 0; margin-bottom: 0"><font color="#E4C192">m</font></p>
-
-  <p align="center" class="Estilo1" style="word-spacing: 0; margin-top: 0; margin-bottom: 0;"><b>Esta
-  página no se encuentra disponible momentaneamente por actualización.</b></p>
-  <p align="center" class="Estilo1" style="word-spacing: 0;"><strong>Estamos trabajando en una nueva versi&oacute;n del aplicativo que estar&aacute; operativo desde el 07/07/2014</strong></p>
-  <p align="center" class="Estilo1" style="word-spacing: 0;"><strong>Para interiorizarse de las nuevas prestaciones del aplicativo puede consultar los siguientes manuales </strong></p>
-  <p style="word-spacing: 0;" align="center"><a href="manual_aplicativo_ddjj.pdf">Manual Aplicativo </a></p>
-  <p style="word-spacing: 0;" align="center"><a href="instrucGrandes.pdf">Manual Grandes Empresas </a></p>
-  <p align="center" class="Estilo1" style="word-spacing: 0"><font face="Verdana"><b>Sepa
-    disculpar las molestias. Muchas gracias.</b></font></p>
+  <p align="center" style="margin-bottom: 0"><img border="0" src="img/top.jpg" width="700" height="120"></p>
+  <p align="center">&nbsp;</p>
+  <table border="0" width="100%">
+    <tr>
+      <td width="19%"></td>
+      <td width="60%" align="right"><div align="center" class="Estilo1">
+        <p>AVISO: HAY UNA NUEVA VERSI&Oacute;N DEL APLICATIVO ONLINE PARA LA GENERACION DE BOLETAS DE PAGO DEL SEGURO DE VIDA Y SEPELIO.</p>
+        <p>PARA INGRESAR HAGA CLICK EN EL SIGUIENTE LINK </p>
+        <p><a href="NuevaVersion/login.php"><strong><font color="#62641A" size="2" face="Verdana">NUEVA VERSION APLICATIVO ONLINE </font></strong></a></p>
+      </div></td>
+      <td width="21%"></td>
+    </tr>
+  </table>
 </body>
 
 </html>
