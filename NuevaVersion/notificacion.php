e6d18d1b5f1bab112fd25877b075f1e0c7007065
tanomartin <tanomartin05@gmail.com>
2017-06-12 12:15:39 -0300

    arreglo notificaciones

--- NuevaVersion/notificacion.php
+++ NuevaVersion/notificacion.php
@@ -3,12 +3,13 @@ session_start();
 
 include('lib/php/verificaSesion.php');
 include('lib/php/verificaConexion.php');
-include('lib/php/funciones.php');
 $root = '';
 // Incluyo el template engine
 include('includes/templateEngine.inc.php');
 
 $id = $_GET['id'];
+$fechalectura = date("Y-m-d H:i:s");
+$sqlLeida = "UPDATE notificaciones SET leida = 1, fechalectura = '$fechalectura' WHERE id = ?";
 
 try {
 	$consultaNotificacion = "SELECT id, tiponotificacion, DATE_FORMAT(fechanotificacion,'%d-%m-%Y') as fechanotificacion, asunto, mensaje, leida FROM notificaciones WHERE id = $id";
@@ -20,20 +21,20 @@ try {
 			$notificacion = array('id' => $id, 'tiponotificacion' => $tiponotificacion, 'fechanotificacion' => $fechanotificacion, 'asunto' => $asunto, 'mensaje' => $mensaje, 'leida' => $leida);
 			if ($leida == 0) {
 				$_SESSION['noleidos'] -= 1;
-				$fechalectura = date("Y-m-d H:i:s");
-				$sqlLeida = "UPDATE notificaciones SET leida = 1, fechalectura = '$fechalectura' WHERE id = ?";
-				if ($stmt = $mysqli->prepare($sqlLeida)) {
-					$stmt->bind_param('i', $id);
-					$stmt->execute();
-					$stmt->close();
-				}
-			} 
+			}
 		}
 	}
+	if ($leida == 0) {
+		if ($stmt = $mysqli->prepare($sqlLeida)) {
+			$stmt->bind_param('i', $id);
+			$stmt->execute();
+			$stmt->close();
+		}
+	}
+	
 	
 	$twig->display('notificacion.html',array("noleidos" => $_SESSION['noleidos'], "userName" => $_SESSION['userNombre'], "notificacion" => $notificacion));
 } catch(Exception $e){
 	$mysqli->rollback();
 	die("ERROR MYSQLI: <br>".$e->getMessage() );
 }
-?>
