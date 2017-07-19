9893faef6b5f49dc9aa0f73f7ae46387bd858abb
tanomartin <tanomartin05@gmail.com>
2017-07-19 12:45:40 -0300

    agregado de impresion de ticket en pupUP

--- NuevaVersion/mostrarTicket.php
+++ NuevaVersion/mostrarTicket.php
@@ -5,7 +5,6 @@ include('lib/php/verificaConexion.php');
 $root = '';
 include('includes/templateEngine.inc.php');
 
-
 $cuit = $_SESSION['userCuit'];
 $referencia = $_GET['ref'];
 $sqlConsuDDJJ = "SELECT sum(totapo)+sum(recarg) as importe FROM ddjjcondocu d, vinculadocu v WHERE v.referencia = '$referencia' and v.nrcuit = '$cuit' and v.nrctrl = d.nrctrl and v.nrcuit = d.nrcuit and d.nrcuil = '99999999999'";
@@ -17,6 +16,9 @@ if ($consuddjj = $mysqli->prepare($sqlConsuDDJJ)) {
 	}
 }
 
-$twig->display('ticketTransferencia.html',array("noleidos" => $_SESSION['noleidos'], "userName" => $_SESSION['userNombre'], "cuit"=>$cuit, "referencia" => $referencia, "importe" => $importe));
-
+if (isset($_GET['imprimir'])) {
+	$twig->display('imprimirTicket.html',array("noleidos" => $_SESSION['noleidos'], "userName" => $_SESSION['userNombre'], "cuit"=>$cuit, "referencia" => $referencia, "importe" => $importe));
+} else {
+	$twig->display('ticketTransferencia.html',array("noleidos" => $_SESSION['noleidos'], "userName" => $_SESSION['userNombre'], "cuit"=>$cuit, "referencia" => $referencia, "importe" => $importe));
+}
 ?>
